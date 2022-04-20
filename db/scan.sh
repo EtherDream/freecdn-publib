f8cb4310a1df6dd03a08ebb579552e8e3cc2819a
zjcqoo <zjcqoo@gmail.com>
2022-04-20 14:55:48 +0800

    更新扫描逻辑。启动时先导入之前遗留的所有记录，而不是运行到那个站点时再导入

--- db/scan.sh
+++ db/scan.sh
@@ -9,32 +9,48 @@ else
   SQL_O=scan-export-3rd.sql
 fi
 
-export bit=0
+export bit=1
 export output=""
 
 mkdir -p var/input var/output
+
+for site in ${SITES[@]}; do
+  output=var/output/$site
+
+  if [ -s $output ]; then
+    echo "import last result: $site ($bit)"
+    wc -l $output
+
+    sql=$(envsubst < $SQL_I)
+    mysql --local-infile=1 <<< "$sql"
+    rm -f $output
+  fi
+  bit=$(($bit * 2))
+done
+
 
 for t in {1..10}; do
   echo "turn: $t"
   bit=1
   for site in ${SITES[@]}; do
+    # 扫描指定的站点
+    if [[ "$1" != "" && "$1" != "init" && "$1" != "$site" ]]; then
+      bit=$(($bit * 2))
+      continue
+    fi
     echo "site: $site ($bit)"
 
     input=var/input/$site
     output=var/output/$site
 
-    if [ -s $output ]; then
-      echo "found last result"
-    else
-      echo "export records ..."
-      sql=$(envsubst < $SQL_O)
-      mysql --skip-column-names <<< "$sql" > $input
-      wc -l $input
+    echo "export records ..."
+    sql=$(envsubst < $SQL_O)
+    mysql --skip-column-names <<< "$sql" > $input
+    wc -l $input
 
-      echo "scan ..."
-      node scan.js $site $input $output
-      wc -l $output
-    fi
+    echo "scan ..."
+    node scan.js $site $input $output
+    wc -l $output
 
     echo "import records ..."
     sql=$(envsubst < $SQL_I)
